c9be18af74f74bd6d0b74db129face8a62c9a495
David Chung <76574620+chungdavid@users.noreply.github.com>
2024-05-06 00:32:31 -0400

    minor change to member variable names

--- dsa_cpp/include/data_structures/LinkedList.hpp
+++ dsa_cpp/include/data_structures/LinkedList.hpp
@@ -25,29 +25,29 @@ public:
     void print();
     
 private:
-    Node<T>* head;
-    int size;
+    Node<T>* head_;
+    int size_;
 };
 
 
 template<typename T>
-LinkedList<T>::LinkedList() : head(new Node<T>(T())), size(0) { }
+LinkedList<T>::LinkedList() : head_(new Node<T>(T())), size_(0) { }
 
 template<typename T>
 LinkedList<T>::~LinkedList() {
-    while(head) {
-        Node<T>* next_node = head->next;
-        delete head;
-        head = next_node;
+    while(head_) {
+        Node<T>* next_node = head_->next;
+        delete head_;
+        head_ = next_node;
     }
 }
 
 template<typename T>
 T LinkedList<T>::getVal(int index) {
-    if(index < 0 || index >= size) {
+    if(index < 0 || index >= size_) {
         throw std::out_of_range("Index out of range");
     } else {
-        Node<T>* curr = head->next;
+        Node<T>* curr = head_->next;
         for(int i = 0; i < index; ++i) {
             curr = curr->next;
         }
@@ -57,34 +57,34 @@ T LinkedList<T>::getVal(int index) {
 
 template<typename T>
 int LinkedList<T>::getSize() {
-    return size;
+    return size_;
 }
 
 template<typename T>
 void LinkedList<T>::insertStart(T val) {
     Node<T>* new_node = new Node<T>(val);
-    new_node->next = head->next;
-    head->next = new_node;
-    ++size;
+    new_node->next = head_->next;
+    head_->next = new_node;
+    ++size_;
 }
 
 template<typename T>
 void LinkedList<T>::insertEnd(T val) {
     Node<T>* new_node = new Node<T>(val);
-    Node<T>* curr = head;
+    Node<T>* curr = head_;
     while(curr->next) {
         curr = curr->next;
     }
     curr->next = new_node;
-    ++size;
+    ++size_;
 }
 
 template<typename T>
 void LinkedList<T>::remove(int index) {
-    if(index >= size || index < 0) {
+    if(index >= size_ || index < 0) {
         throw std::out_of_range("Index out of range.");
     } else {
-        Node<T>* curr = head;
+        Node<T>* curr = head_;
         for(int i = 0; i < index; ++i) {
             curr = curr->next;
         }
@@ -92,12 +92,12 @@ void LinkedList<T>::remove(int index) {
         curr->next = to_delete->next;
         delete(to_delete);
     }
-    --size;
+    --size_;
 }
 
 template<typename T>
 void LinkedList<T>::print() {
-    Node<T>* curr = head;
+    Node<T>* curr = head_;
     while(curr->next) {
         std::cout << curr->next->val << "  ";
         curr=curr->next;
