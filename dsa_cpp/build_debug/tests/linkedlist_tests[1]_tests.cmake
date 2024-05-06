9d88ad3fb104a75f8937f5c9b224eb1073f81140
David Chung <76574620+chungdavid@users.noreply.github.com>
2024-05-06 00:39:21 -0400

    remove build folder that was accidently pushed

--- dsa_cpp/build_debug/tests/linkedlist_tests[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[LinkedListTest.GetValTest]=]  /home/apollo/cpp-data-structures-algorithms/dsa_cpp/build_debug/tests/linkedlist_tests [==[--gtest_filter=LinkedListTest.GetValTest]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[LinkedListTest.GetValTest]=]  PROPERTIES WORKING_DIRECTORY /home/apollo/cpp-data-structures-algorithms/dsa_cpp/build_debug/tests SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  linkedlist_tests_TESTS LinkedListTest.GetValTest)
