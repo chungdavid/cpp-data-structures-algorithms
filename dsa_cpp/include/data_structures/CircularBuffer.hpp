32e257f38cf9122f88e0d9536033ecd6562a0404
David Chung <76574620+chungdavid@users.noreply.github.com>
2024-05-07 14:39:26 -0400

    implement circular buffer

--- dsa_cpp/include/data_structures/CircularBuffer.hpp
+++ dsa_cpp/include/data_structures/CircularBuffer.hpp
@@ -23,38 +23,43 @@ private:
 };
 
 template<typename T, size_t N>
-CircularBuffer<T, N>::CircularBuffer() {
-    //TODO
-}
+CircularBuffer<T, N>::CircularBuffer() : size_(0), front_(0), back_(-1) { }
 
 template<typename T, size_t N>
-CircularBuffer<T, N>::~CircularBuffer() {
-    //TODO
-}
+CircularBuffer<T, N>::~CircularBuffer() { }
 
 template<typename T, size_t N>
 size_t CircularBuffer<T, N>::getSize() const {
-    //TODO
-    return 100;
+    return size_;
 }
 
 template<typename T, size_t N>
 bool CircularBuffer<T, N>::isEmpty() const {
-    //TODO
-    return false;
+    return size_ == 0;
 }
 
 template<typename T, size_t N>
 void CircularBuffer<T, N>::push(T val) {
-    //TODO
+    back_ = (back_ + 1) % N;
+    buffer_[back_] = val;
+    if(size_ < N) {
+        ++size_;
+    } else {
+        front_ = (front_ + 1) % N;
+    }
 }
 
 template<typename T, size_t N>
 T CircularBuffer<T, N>::pop() {
-    //TODO
-    return T();
+    if(size_ == 0) {
+        throw std::out_of_range("Buffer is empty, cannot pop from buffer.");
+    } else {
+        T front_val = buffer_[front_];
+        front_ = (front_ + 1) % N;
+        --size_;
+        return front_val;
+    }
 }
 
 
-
-} //nameaspace dsa_cpp
+} //namespace dsa_cpp
